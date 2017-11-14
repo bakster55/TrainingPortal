e695cb666850c029b2cadeab4aac62ac0431de5c
Stas Kuropatkin <anabi1z@mail.ru>
2017-11-14 15:07:12 +0300

    [TP-1] Empty ASP.NET MVC project.

---
+++ TrainingPortal/TrainingPortal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrainingPortal.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
